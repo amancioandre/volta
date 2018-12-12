f6705bfe102ad93731e544f5d082ec46a8a7934a
Gabriel Sicuto <gabrielsicuto@gmail.com>
2018-12-12 18:18:40 -0200

    ajust search

--- src/components/Forms/FormPerson/FormPerson.js
+++ src/components/Forms/FormPerson/FormPerson.js
@@ -1,73 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import PrimaryInfo from '../FormBuildings/PrimaryInfo/PrimaryInfo';
-import SecundaryInfo from '../FormBuildings/SecundaryInfo/SecundaryInfo';
-import SubmitButton from '../FormBuildings/SubmitButton/SubmitButton';
-import Background from '../FormBuildings/Background/Background';
-import Appearance from '../FormBuildings/Appearance/Appearance';
-import Health from '../FormBuildings/Health/Health';
-import Documents from '../FormBuildings/Documents/Documents';
-import { Form } from 'reactstrap';
-import CrudServices from '../../../crud/crud-services';
+import PrimaryInfo from "../FormBuildings/PrimaryInfo/PrimaryInfo";
+import SecundaryInfo from "../FormBuildings/SecundaryInfo/SecundaryInfo";
+import SubmitButton from "../FormBuildings/SubmitButton/SubmitButton";
+import Background from "../FormBuildings/Background/Background";
+import Appearance from "../FormBuildings/Appearance/Appearance";
+import Health from "../FormBuildings/Health/Health";
+import Documents from "../FormBuildings/Documents/Documents";
+import { Form } from "reactstrap";
+import CrudServices from "../../../crud/crud-services";
 
-import {
-	withRouter
-} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 // CSS
 // import './FormSignUp.css';
 
 class FormPerson extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={ person:{
-        geoReferences: [{ lat: 0, lng: 0 }],
-      }
+    this.state = {
+      geoReferences: []
     };
     this.service = new CrudServices();
     this.handleChange = this.handleChange.bind(this);
     this.handleCreatePerson = this.handleCreatePerson.bind(this);
     this.getPosition = this.getPosition.bind(this);
     this.getPositionHandler = this.getPositionHandler.bind(this);
-  };
+  }
 
   handleCreatePerson(event) {
     event.preventDefault();
-    const person = this.person.state;
-    console.log(person);
+    const person = this.state;
+    console.log(this.state);
     this.service
       .createPerson(person)
       .then(response => {
         this.setState({});
+        this.service
+          .addPosition(
+            this.props.match.params.personId,
+            this.state.geoReferences
+          )
+          .then(res => console.log("Result------->", res));
       })
       .catch(error => console.log(error));
   }
 
   getPosition() {
-    navigator.geolocation.getCurrentPosition((pos) => {
+    navigator.geolocation.getCurrentPosition(pos => {
       let { coords } = pos;
-      console.log('Pos -------->',coords);
+      console.log("Pos -------->", coords);
       this.getPositionHandler(coords);
     });
   }
 
   getPositionHandler(coords) {
     const newPos = { lat: coords.latitude, lng: coords.longitude };
-    console.log('this.state --------> ', this.person.state);
-    const { geoReferences } = this.person.state;
+    console.log("this.state --------> ", this.state);
+    const { geoReferences } = this.state;
     geoReferences.push(newPos);
-
-    this.setState({ geoReferences }, () => {
-      this.service.addPosition(this.props.match.params.personId, this.preson.state.geoReferences)
-        .then(res => (console.log('Result------->', res)));
-    })
   }
 
-  handleChange (event) {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-    console.log(this.person.state);
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    console.log(this.state);
   }
 
   componentDidMount() {
@@ -75,20 +72,18 @@ class FormPerson extends Component {
   }
 
   render() {
-    console.log(this.state);
-    return(
-      <Form onSubmit= {this.handleCreatePerson}>
-        <PrimaryInfo state = { this.person.state } change = { this.handleChange }/>
-        <SecundaryInfo state = { this.person.state } change = { this.handleChange }/>
-        <Background state = { this.person.state } change = { this.handleChange }/>
-        <Appearance state = { this.person.state } change = { this.handleChange }/>
-        <Health state = { this.person.state } change = { this.handleChange }/>
-        <Documents state = { this.person.state } change = { this.handleChange }/>
-        <SubmitButton buttonName = 'Create'/>
+    return (
+      <Form onSubmit={this.handleCreatePerson}>
+        <PrimaryInfo state={this.state} change={this.handleChange} />
+        <SecundaryInfo state={this.state} change={this.handleChange} />
+        <Background state={this.state} change={this.handleChange} />
+        <Appearance state={this.state} change={this.handleChange} />
+        <Health state={this.state} change={this.handleChange} />
+        <Documents state={this.state} change={this.handleChange} />
+        <SubmitButton buttonName="Create" />
       </Form>
-    )
+    );
   }
-
 }
 
 export default withRouter(FormPerson);
