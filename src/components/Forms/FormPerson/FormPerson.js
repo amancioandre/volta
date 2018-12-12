d07179917bfa72370a279d15168ca6d997409772
Gabriel Sicuto <gabrielsicuto@gmail.com>
2018-12-12 15:40:17 -0200

    ajust on Form person to include geoLocation

--- src/components/Forms/FormPerson/FormPerson.js
+++ src/components/Forms/FormPerson/FormPerson.js
@@ -22,12 +22,13 @@ class FormPerson extends Component {
   constructor(props){
     super(props);
     this.state = {
-      lat: 0,
-      lng: 0,
+      geoReferences: [{ lat: 0, lng: 0 }],
     };
     this.service = new CrudServices();
     this.handleChange = this.handleChange.bind(this);
     this.handleCreatePerson = this.handleCreatePerson.bind(this);
+    this.getPosition = this.getPosition.bind(this);
+    this.getPositionHandler = this.getPositionHandler.bind(this);
   };
 
   handleCreatePerson(event) {
@@ -42,13 +43,38 @@ class FormPerson extends Component {
       .catch(error => console.log(error));
   }
 
+  getPosition() {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let { coords } = pos;
+      console.log('Pos -------->',coords);
+      this.getPositionHandler(coords);
+    });
+  }
+
+  getPositionHandler(coords) {
+    const newPos = { lat: coords.latitude, lng: coords.longitude };
+    console.log('this.state --------> ', this.state);
+    const { geoReferences } = this.state;
+    geoReferences.push(newPos);
+
+    this.setState({ geoReferences }, () => {
+      this.service.addPosition(this.props.match.params.personId, this.state.geoReferences)
+        .then(res => (console.log('Result------->', res)));
+    })
+  }
+
   handleChange (event) {  
     const {name, value} = event.target;
     this.setState({[name]: value});
     console.log(this.state);
   }
 
+  componentDidMount() {
+    this.getPosition();
+  }
+
   render() {
+    console.log(this.state);
     return(
       <Form onSubmit= {this.handleCreatePerson}>
         <PrimaryInfo state = { this.state } change = { this.handleChange }/>
